1a9c3703adeaa45cff85d5c65fabd4b82c6a7885
Jared Hall <jared@pyscape.com>
2016-08-10 11:00:32 -0600

    added minification script and combined sources to cut down on the number of requests. Also added a favorite icon directly into the head of the index page. fixed a few HTML validation errors, too.

---
+++ bin/minify.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+# Requires java and yui-compressor
+# https://github.com/yui/yuicompressor/downloads
+#
+# Consider adding a sym link to the ui-compressor if it's not already 
+# setup in /usr/bin/. Something like the following should work:
+# >>> sudo ln -s /home/myUser/source/yuicompressor-2.4.7 /usr/bin/yuicompressor
+# Or modify this file to point to your yuicompressor
+
+yui='/usr/bin/yuicompressor/build/yuicompressor-2.4.7.jar'
+java='/usr/bin/java'
+
+###################
+# JS Minification #
+###################
+
+# Must have. Do not remove.
+cat js/jquery.min.js > js/deploy.min.js
+echo $'\n' >> js/deploy.min.js
+cat js/bootstrap.min.js >> js/deploy.min.js
+echo $'\n' >> js/deploy.min.js
+
+# Optional
+cat js/owl.carousel.min.js >> js/deploy.min.js
+echo $'\n' >> js/deploy.min.js
+cat js/index.js >> js/deploy.min.js
+
+####################
+# CSS minification #
+####################
+
+# Must have. Do not remove.
+
+
+$java -jar $yui css/theme.css -o css/theme.min.css
+$java -jar $yui css/animate.css -o css/animate.min.css
+
+cat css/theme.min.css > css/style.min.css
+echo $'\n' >> css/style.min.css
+cat css/animate.min.css >> css/style.min.css
---
+++ js/index.js
@@ -0,0 +1,16 @@
+$(function() {
+    // Init the Owl Carousel slider script - remove this code if it is not in use
+    $('.owl-carousel').owlCarousel({
+        animateOut: 'fadeOut',
+        autoHeight:true,
+        items:1,
+        loop: true,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        margin:0,
+        stagePadding:0,
+        nav: false,
+        smartSpeed:0
+
+    });
+});
